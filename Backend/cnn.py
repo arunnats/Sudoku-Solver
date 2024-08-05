6464defa45b49e2175f943433d5d2a994a22748f
arunnats <arun_b220019cs@nitc.ac.in>
2024-08-05 22:18:10 +0530

    still tweaking

--- Backend/cnn.py
+++ Backend/cnn.py
@@ -71,5 +71,5 @@ plt.show()
 model_json = model.to_json()
 with open("model.json", "w") as json_file:
     json_file.write(model_json)
-model.save_weights("model.h5")
+model.save_weights("model.weights.h5")
 print("Saved model to disk")
